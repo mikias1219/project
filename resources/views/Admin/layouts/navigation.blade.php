c81a83fde6c54d8649a6bc4613c83920fea55e87
mikias <mikiyasabate003@gmail.com>
2024-05-10 13:41:42 +0300

    layout added

---
+++ resources/views/Admin/layouts/navigation.blade.php
@@ -0,0 +1,89 @@
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Sidebar -->
+    <div class="sidebar">
+        <!-- Sidebar user panel (optional) -->
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+            <div class="image">
+                <img src="{{ asset('dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
+            </div>
+            {{-- <div class="info">
+                <?php
+                // $representative = Auth::guard('representative')->user();
+                ?>
+                <a href="#" class="d-block">{{ $representative->name }}</a>
+        </div> --}}
+    </div>
+    <!-- Sidebar Menu -->
+    <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+            <!-- Add icons to the links using the .nav-icon class with font-awesome or any other icon font library -->
+            <li class="nav-item">
+                <a href="" class="nav-link">
+                    <i class="nav-icon fas fa-chart-line"></i>
+                    <p>Dashboard</p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="" class="nav-link">
+                    <i class="nav-icon fas fa-clipboard"></i>
+                    <p>Agricultural Resource</p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="" class="nav-link">
+                    <i class="nav-icon fas fa-tasks"></i>
+                    <p>Disease Monitoring</p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="" class="nav-link">
+                    <i class="nav-icon fas fa-bell"></i>
+                    <p>View Announcement</p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="" class="nav-link">
+                    <i class="nav-icon fas fa-shopping-cart"></i>
+                    <p>Place Order</p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="" class="nav-link">
+                    <i class="nav-icon fas fa-share-alt"></i>
+                    <p>Experience Sharing</p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="" class="nav-link">
+                    <i class="nav-icon fas fa-question-circle"></i>
+                    <p>Ask Question</p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="" class="nav-link">
+                    <i class="nav-icon fas fa-user"></i>
+                    <p>My Profile</p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="" class="nav-link">
+                    <i class="nav-icon fas fa-key"></i>
+                    <p>Change Password</p>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                    <i class="nav-icon fas fa-sign-out-alt"></i>
+                    {{ __('Logout') }}
+                </a>
+
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    @csrf
+                </form>
+            </li>
+        </ul>
+    </nav>
+    <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+</aside>
---
+++ resources/views/Expert/layouts/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="main-footer ">
+    <p class="text-center">
+        <strong>Copyright &copy; 2024 </strong>
+        All rights reserved.
+    </p>
+
+
+</footer>
